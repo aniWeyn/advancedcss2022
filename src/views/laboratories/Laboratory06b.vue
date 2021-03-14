0a1c8ade07193cd018aafa0a21de05860eb96b21
Sabina Niewiadomska <ext_sabina.niewiadomska@aprila.no>
2021-03-14 22:44:11 +0100

    laboratories

---
+++ src/views/laboratories/Laboratory06b.vue
@@ -0,0 +1,294 @@
+<template>
+  <div class="container">
+    <div>
+      <h2>CSS Transform</h2>
+    </div>
+    <div>
+      <h4>Example of Matrix</h4>
+      <iframe
+        height="600"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-matrix"
+        src="https://codepen.io/AniWeyn/embed/rNWRWLd?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/rNWRWLd"
+          >CSS-Transition--03-transform-matrix</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Matrix3d</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--02-transform-matrix3d"
+        src="https://codepen.io/AniWeyn/embed/zYobowJ?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/zYobowJ"
+          >CSS-Transition--02-transform-matrix3d</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Translate</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-translate-01"
+        src="https://codepen.io/AniWeyn/embed/xxRBRLM?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/xxRBRLM"
+          >CSS-Transition--03-transform-translate-01</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of TranslateY</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-translate-02"
+        src="https://codepen.io/AniWeyn/embed/YzpgpEz?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/YzpgpEz"
+          >CSS-Transition--03-transform-translate-02</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Translate with two values</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-translate-04"
+        src="https://codepen.io/AniWeyn/embed/RwodoxP?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/RwodoxP"
+          >CSS-Transition--03-transform-translate-04</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Translate3d</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-translate3d"
+        src="https://codepen.io/AniWeyn/embed/JjbzbMV?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/JjbzbMV"
+          >CSS-Transition--03-transform-translate3d</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Scale down</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-scale"
+        src="https://codepen.io/AniWeyn/embed/rNWRWrq?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/rNWRWrq"
+          >CSS-Transition--03-transform-scale</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Scale up</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-scale-02"
+        src="https://codepen.io/AniWeyn/embed/Rwodoea?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/Rwodoea"
+          >CSS-Transition--03-transform-scale-02</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of ScaleX</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-scale-03"
+        src="https://codepen.io/AniWeyn/embed/GRNeNwj?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/GRNeNwj"
+          >CSS-Transition--03-transform-scale-03</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of ScaleY</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-scale-03"
+        src="https://codepen.io/AniWeyn/embed/eYBXBbQ?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/eYBXBbQ"
+          >CSS-Transition--03-transform-scale-03</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Rotate with positive value</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-rotate"
+        src="https://codepen.io/AniWeyn/embed/NWbJbow?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/NWbJbow"
+          >CSS-Transition--03-transform-rotate</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Rotate with negative value</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-rotate"
+        src="https://codepen.io/AniWeyn/embed/poNYNBK?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/poNYNBK"
+          >CSS-Transition--03-transform-rotate</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Rotate and TransalteX at the same time</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-rotate"
+        src="https://codepen.io/AniWeyn/embed/dyOrOEY?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/dyOrOEY"
+          >CSS-Transition--03-transform-rotate</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Rotate3d</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-rotate3d"
+        src="https://codepen.io/AniWeyn/embed/vYyPyqK?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/vYyPyqK"
+          >CSS-Transition--03-transform-rotate3d</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+      <h4>Example of Skew</h4>
+      <iframe
+        height="450"
+        style="width: 100%"
+        scrolling="no"
+        title="CSS-Transition--03-transform-skew"
+        src="https://codepen.io/AniWeyn/embed/rNWRWXR?height=265&theme-id=dark&default-tab=result"
+        frameborder="no"
+        loading="lazy"
+        allowtransparency="true"
+        allowfullscreen="true"
+      >
+        See the Pen
+        <a href="https://codepen.io/AniWeyn/pen/rNWRWXR"
+          >CSS-Transition--03-transform-skew</a
+        >
+        by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+    </div>
+  </div>
+</template>
