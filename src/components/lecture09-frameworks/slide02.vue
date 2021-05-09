5c3db2148a0f279aaff41b5e12dc08da3419274e
Sabina Niewiadomska <ext_sabina.niewiadomska@aprila.no>
2021-05-09 14:50:48 +0200

    added lecture 9

---
+++ src/components/lecture09-frameworks/slide02.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slide">
+    <h3>CSS Frameworks</h3>
+    <h4>Bootstrap</h4>
+    <a target="_blank" href="https://getbootstrap.com/"
+      >https://getbootstrap.com/</a
+    >
+    <h4>Foundation</h4>
+    <a target="_blank" href="https://get.foundation/"
+      >https://get.foundation/</a
+    >
+    <h4>Bulma</h4>
+    <a target="_blank" href="https://bulma.io/">https://bulma.io/</a>
+    <h4>Tailwind</h4>
+    <a target="_blank" href="https://tailwindcss.com/"
+      >https://tailwindcss.com/</a
+    >
+  </div>
+</template>
--- src/views/lectures/Lecture09.vue
+++ src/views/lectures/Lecture09.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="lecture">
+    <Slide00 v-if="render === 0" />
+    <Slide01 v-if="render === 1" />
+    <Slide02 v-if="render === 2" />
+    <Slide03 v-if="render === 3" />
+    <Slide04 v-if="render === 4" />
+    <Slide05 v-if="render === 5" />
     <div class="button-navigation">
       <div>
         <button v-if="render != 0" v-on:click="render -= 1">Back</button>
@@ -12,9 +18,23 @@
 </template>
 
 <script>
+import Slide00 from "@/components/lecture09-frameworks/slide00.vue";
+import Slide01 from "@/components/lecture09-frameworks/slide01.vue";
+import Slide02 from "@/components/lecture09-frameworks/slide02.vue";
+import Slide03 from "@/components/lecture09-frameworks/slide03.vue";
+import Slide04 from "@/components/lecture09-frameworks/slide04.vue";
+import Slide05 from "@/components/lecture09-frameworks/slide05.vue";
+
 export default {
-  name: "Lecture00",
-  components: {},
+  name: "Lecture09",
+  components: {
+    Slide00,
+    Slide01,
+    Slide02,
+    Slide03,
+    Slide04,
+    Slide05,
+  },
   data() {
     return {
       render: 0,
