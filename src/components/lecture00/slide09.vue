2d5fc3b249574be39a7db62fa8a6fc846a092bb2
s4bina <sabina@zoopit.no>
2021-11-19 21:52:50 +0100

    changes

--- src/components/lecture00/slide09.vue
+++ src/components/lecture00/slide09.vue
@@ -5,7 +5,8 @@
         <h2 class="rotate">Assignment 3</h2>
       </div>
       <div>
-        deadline: 19.04, 08:15
+        <h3>Final assignments</h3>
+        deadline: 25.04, 08:15
         <p class="space">Project - production release</p>
         <p class="space">
           In Assignment 3 your group is expected to deliver production ready
@@ -18,44 +19,17 @@
         </p>
         <p class="space">
           The project should have an active repository with regulary added PR on
-          GitHub/GitLab, project should be released through GitHub/GitLab pages,
-          your Lighthouse result must be improved.
+          GitHub and used Project board, the assignment should be released through GitHub pages.
         </p>
-        <p class="space">
-          And around 1-1.5 page of reflection and justification of your group
-          choices.
-        </p>
-        <p class="space">
+
+      </div>
+      <div>
+        <p class="space font--small">
           The final project will be presented to other groups during the
           lecture. Students from other courses as well as teachers might be
           invited for a final presentation. During final presentation you could
           gather wider feedback on your work.
         </p>
-      </div>
-      <div>
-        <ul class="font--small">
-          <li class="past">January 18 - Lecture: Introduction</li>
-          <li class="past">January 25 - Advanced CSS</li>
-          <li class="past">
-            February 1 - Assignment 0 (not graded), Blackboard
-          </li>
-          <li class="past">
-            February 1 - Lecture: UX and Storytelling workshops
-          </li>
-          <li class="past">
-            February 15 - Assignment 1 (pass/fail), Blackboard
-          </li>
-          <li class="past">February 15 - Lecture: Advanced CSS</li>
-          <li class="past">March 1 - Lecture: Advanced CSS</li>
-          <li class="past">March 15 - Lecture: Advanced CSS</li>
-          <li class="past">March 22 - Lecture: Advanced CSS</li>
-          <li class="past">April 05 - Assignment 2 (pass/fail), Blackboard</li>
-          <li class="past">April 12 - Lecture: Advanced CSS</li>
-          <li class="bold">April 19 - Assignment 3 (pass/fail), Blackboard</li>
-          <li>April 26 - Lecture: Advanced CSS</li>
-          <li class="bold">April 26 or May 03 - Final Presentation</li>
-          <li>May, 14 - Individual report</li>
-        </ul>
       </div>
     </div>
   </div>
