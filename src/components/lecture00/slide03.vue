3a7241b26806a661bc620a78e97e8fd19fc0b5a9
s4bina <sabina@zoopit.no>
2021-11-19 19:08:41 +0100

    updated timeline

--- src/components/lecture00/slide03.vue
+++ src/components/lecture00/slide03.vue
@@ -2,21 +2,25 @@
   <div class="slide">
     <h2>Timeline</h2>
     <ul>
-      <li>January 18 - Lecture: Introduction</li>
-      <li>January 25 - Lecture: Advanced CSS</li>
-      <li>February 1 - Assignment 0 (not graded), Blackboard</li>
-      <li>February 1 - Lecture: UX and Storytelling workshops</li>
-      <li>February 15 - Assignment 1 (pass/fail), Blackboard</li>
-      <li>February 15 - Lecture: Advanced CSS</li>
-      <li>March 1 - Lecture: Advanced CSS</li>
-      <li>March 15 - Lecture: Advanced CSS</li>
-      <li>March 22 - Lecture: Advanced CSS</li>
-      <li>April 05 - Assignment 2 (pass/fail), Blackboard</li>
-      <li>April 12 - Lecture: Advanced CSS</li>
-      <li>April 19 - Assignment 3 (pass/fail), Blackboard</li>
-      <li>April 26 - Lecture: Advanced CSS</li>
-      <li>April 26 or May 03 - Final Presentation</li>
-      <li>May, 14 - Individual report</li>
+      <li>January 10 - L1: Introduction + CSS Selectors (w2)</li>
+      <li>January 11 - L2: Animating CSS (w2)</li>
+      <li>January 12 - L3: Animating SVG (w2)</li>
+      <li>January 13 - L4: CSS Grid, CSS Units, Text Effects (w2)</li>
+      <li>January 14 - L5: Flexbox + Simpel JS animations (w2)</li>
+      <li>January 14 - Assignment 0 - group contract (w2)</li>
+      <li>January 31 - Assignment 1 - group work on CodePen (w)</li>
+      <li>February 14 - L6: Designing for animation / Storytelling / Accessibility (w7)</li>
+      <li>February 15 - L7: Working with Visual Studio Code and GitHub and Project Management (w7)</li>
+      <li>February 21 - Group Work (w8)</li>
+      <li>February 28 - Group Work (w9)</li>
+      <li>March 07 - Assignment 2 - group work design, project plan, GitHub (w)</li>
+      <li>March 14 - L8: Architecting CSS + Preprocessors... and SASS + CSS Variables vs. SASS variables (w11)</li>
+      <li>March 15 - L9: Guest Lecture Michal (w11)</li>
+      <li>March 21 - Group Work (w12)</li>
+      <li>March 28 - Group Work (w13)</li>
+      <li>April 04 - L10: CSS Frameworks + Project feedback (w14)</li>
+      <li>April 25 - Assignmnet 3 - final project</li>
+      <li>April 25 - L11: TBA, Final Presentation (w17)</li>
     </ul>
   </div>
 </template>
