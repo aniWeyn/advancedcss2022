12f174b3a6aaf5c57f3029eed8584a73ffab5d5e
s4bina <sabina@zoopit.no>
2021-11-19 18:44:54 +0100

    removed year

--- src/components/lecture00/slide02.vue
+++ src/components/lecture00/slide02.vue
@@ -2,21 +2,21 @@
   <div class="slide">
     <h2>Course organisation</h2>
     <ol>
-      <li>January 10, 2022 - L1: Introduction + CSS Selectors (w2)</li>
-      <li>January 11, 2022 - L2: Animating CSS (w2)</li>
-      <li>January 12, 2022 - L3: Animating SVG (w2)</li>
-      <li>January 13, 2022 - L4: CSS Grid, CSS Units, Text Effects (w2)</li>
-      <li>January 14, 2022 - L5: Flexbox + Simpel JS animations (w2)</li>
-      <li>February 14, 2022 - L6: Designing for animation / Storytelling / Accessibility (w7)</li>
-      <li>February 15, 2022 - L7: Working with Visual Studio Code and GitHub and Project Management (w7)</li>
-      <li>February 21, 2022 - Group Work (w8)</li>
-      <li>February 28, 2022 - Group Work (w9)</li>
-      <li>March 14, 2022 - L8: Architecting CSS + Preprocessors... and SASS + CSS Variables vs. SASS variables (w11)</li>
-      <li>March 15, 2022- L9: Guest Lecture Michal (w11)</li>
-      <li>March 21, 2022- Group Work (w12)</li>
-      <li>March 28, 2022- Group Work (w13)</li>
-      <li>April 04, 2022- L10: CSS Frameworks + Project feedback (w14)</li>
-      <li>April 25, 2021- L11: TBA, Final Presentation (w17)</li>
+      <li>January 10 - L1: Introduction + CSS Selectors (w2)</li>
+      <li>January 11 - L2: Animating CSS (w2)</li>
+      <li>January 12 - L3: Animating SVG (w2)</li>
+      <li>January 13 - L4: CSS Grid, CSS Units, Text Effects (w2)</li>
+      <li>January 14 - L5: Flexbox + Simpel JS animations (w2)</li>
+      <li>February 14 - L6: Designing for animation / Storytelling / Accessibility (w7)</li>
+      <li>February 15 - L7: Working with Visual Studio Code and GitHub and Project Management (w7)</li>
+      <li>February 21 - Group Work (w8)</li>
+      <li>February 28 - Group Work (w9)</li>
+      <li>March 14 - L8: Architecting CSS + Preprocessors... and SASS + CSS Variables vs. SASS variables (w11)</li>
+      <li>March 15 - L9: Guest Lecture Michal (w11)</li>
+      <li>March 21 - Group Work (w12)</li>
+      <li>March 28 - Group Work (w13)</li>
+      <li>April 04 - L10: CSS Frameworks + Project feedback (w14)</li>
+      <li>April 25 - L11: TBA, Final Presentation (w17)</li>
     </ol>
     <a
       target="_blank"
