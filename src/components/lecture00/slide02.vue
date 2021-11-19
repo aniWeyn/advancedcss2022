e4a9a11df847d74ad3d560606b7195992070e146
s4bina <sabina@zoopit.no>
2021-11-19 18:43:54 +0100

    course organisation update

--- src/components/lecture00/slide02.vue
+++ src/components/lecture00/slide02.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="slide">
     <h2>Course organisation</h2>
-    <p class="space">10 lectures</p>
+    <ol>
+      <li>January 10, 2022 - L1: Introduction + CSS Selectors (w2)</li>
+      <li>January 11, 2022 - L2: Animating CSS (w2)</li>
+      <li>January 12, 2022 - L3: Animating SVG (w2)</li>
+      <li>January 13, 2022 - L4: CSS Grid, CSS Units, Text Effects (w2)</li>
+      <li>January 14, 2022 - L5: Flexbox + Simpel JS animations (w2)</li>
+      <li>February 14, 2022 - L6: Designing for animation / Storytelling / Accessibility (w7)</li>
+      <li>February 15, 2022 - L7: Working with Visual Studio Code and GitHub and Project Management (w7)</li>
+      <li>February 21, 2022 - Group Work (w8)</li>
+      <li>February 28, 2022 - Group Work (w9)</li>
+      <li>March 14, 2022 - L8: Architecting CSS + Preprocessors... and SASS + CSS Variables vs. SASS variables (w11)</li>
+      <li>March 15, 2022- L9: Guest Lecture Michal (w11)</li>
+      <li>March 21, 2022- Group Work (w12)</li>
+      <li>March 28, 2022- Group Work (w13)</li>
+      <li>April 04, 2022- L10: CSS Frameworks + Project feedback (w14)</li>
+      <li>April 25, 2021- L11: TBA, Final Presentation (w17)</li>
+    </ol>
     <a
       target="_blank"
       href="https://www.ntnu.edu/studies/courses/IDG1293#tab=timeplan"
       >Timeplan</a
     >
-    <ol>
-      <li>January 18, 2021 - Introduction</li>
-      <li>January 25, 2021 - Advanced CSS</li>
-      <li>February 1, 2021 - UX and Storytelling workshops</li>
-      <li>February 15, 2021 - Advanced CSS</li>
-      <li>March 1, 2021 - Advanced CSS</li>
-      <li>March 15, 2021- Advanced CSS</li>
-      <li>March 22, 2021- Advanced CSS</li>
-      <li>April 12, 2021- Advanced CSS</li>
-      <li>April 26, 2021- Advanced CSS</li>
-      <li>April 26 or May 03, 2021- Final Presentation</li>
-    </ol>
-    <p class="space">
-      If lectures will be hold on Zoom, we might split meetings and hold them
-      every week.
-    </p>
   </div>
 </template>
