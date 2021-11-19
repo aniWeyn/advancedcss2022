1e40fac1558ddb5438a716f5635c3fc3fcc018e3
s4bina <sabina@zoopit.no>
2021-11-19 21:33:09 +0100

    updated Assignment 2

--- src/components/lecture00/slide08.vue
+++ src/components/lecture00/slide08.vue
@@ -5,56 +5,35 @@
         <h2 class="rotate">Assignment 2</h2>
       </div>
       <div>
-        deadline: 05.04, 08:15
-        <p class="space">Project - developer release</p>
+        <h3>Storytelling and design for animation. GitHub project beginning.</h3>
+        deadline: 07.03, 08:15
         <p class="space">
-          In Assignment 2 your group is expected to deliver an animated project
-          where you are experimenting with typography on the web, simple
-          animation, use of svg.
+          In Assignment 2 your group is expected to deliver a design of the project you will later develop as Assignment 3 (your final project). As well as started project on GitHub repository.
         </p>
         <p class="space">
-          Your group should use Sass preprocessor and BEM naming convention, as
-          well as you should apply proposed css architecture.
-        </p>
-        <p class="space">
-          The project should have a repository on GitHub/GitLab, you should use
-          Pull Requests to accept your colleagoues additions to the code,
-          project should be released through GitHub/GitLab pages. You should
-          test your page with Lighthouse.
-        </p>
-        <p class="space">
-          Deliver around 1-1.5 page of reflection and justification of your
-          group choices, add results from the Lighthouse.
-        </p>
-        <p class="space">
-          You will receive feedback to your work both on the development level,
-          as well as about it use experience.
-        </p>
+          Where are you aiming:
+          <ul>
+            <li><a href="https://thanks-in-advance.com/">https://thanks-in-advance.com/</a></li>
+            <li><a href="TBA">TBA</a></li>
+          </ul>
+          
+          </p>
+
       </div>
       <div>
-        <ul class="font--small">
-          <li class="past">January 18 - Lecture: Introduction</li>
-          <li class="past">January 25 - Advanced CSS</li>
-          <li class="past">
-            February 1 - Assignment 0 (not graded), Blackboard
-          </li>
-          <li class="past">
-            February 1 - Lecture: UX and Storytelling workshops
-          </li>
-          <li class="past">
-            February 15 - Assignment 1 (pass/fail), Blackboard
-          </li>
-          <li class="past">February 15 - Lecture: Advanced CSS</li>
-          <li class="past">March 1 - Lecture: Advanced CSS</li>
-          <li class="past">March 15 - Lecture: Advanced CSS</li>
-          <li class="past">March 22 - Lecture: Advanced CSS</li>
-          <li class="bold">April 05 - Assignment 2 (pass/fail), Blackboard</li>
-          <li>April 12 - Lecture: Advanced CSS</li>
-          <li>April 19 - Assignment 3 (pass/fail), Blackboard</li>
-          <li>April 26 - Lecture: Advanced CSS</li>
-          <li>April 26 or May 03 - Final Presentation</li>
-          <li>May, 14 - Individual report</li>
-        </ul>
+        <p class="space font--small">
+          Your project needs to describe important enviromental/health/social issue (see example from Thanks in Advance). 
+          That issue needs to be described in interesting way which tells a story. 
+          When you will be decided about the story it is time to put it into design. 
+          Your design needs to reflect animations and drawings which you will develop in final project.
+          You can prepare designs as drawing or in digital for such as Figma. The most important is that your group will well define your project.
+        </p>
+        <p class="space font--small">
+          Create repostory for Final Project. Start your work on Read.me file, add all group members as contributors, 
+          every group member needs to submit one pull request which has to be reviewd and accepted by two other group members, create a project board with columns (To Do, In Progress, Done).
+          Define your design in small tasks which describe development activity, plan how you will redistirbute work among group members. 
+          Fill To Do column with development tasks and assign group members. 
+        </p>
       </div>
     </div>
   </div>
