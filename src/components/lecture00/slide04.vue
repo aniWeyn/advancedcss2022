6b3f4a340c88827eddba1696ea5326236b03ea69
s4bina <sabina@zoopit.no>
2021-11-19 18:51:36 +0100

    update

--- src/components/lecture00/slide04.vue
+++ src/components/lecture00/slide04.vue
@@ -3,10 +3,10 @@
     <h2>Course work</h2>
     3 Assignments and Individual Report
     <ul>
-      <li>Assignment 0 (not graded), Blackboard, deadline: 01.02, 08:15</li>
-      <li>Assignment 1 (pass/fail), Blackboard, deadline: 15.02, 08:15</li>
-      <li>Assignment 2 (pass/fail), Blackboard, deadline: 05.04, 08:15</li>
-      <li>Assignment 3 (pass/fail), Blackboard, deadline: 19.04, 08:15</li>
+      <li>Assignment 0 (not graded), Blackboard, deadline: 14.01, 08:15</li>
+      <li>Assignment 1 (pass/fail), Blackboard, deadline: 31.01, 08:15</li>
+      <li>Assignment 2 (pass/fail), Blackboard, deadline: 07.03, 08:15</li>
+      <li>Assignment 3 (pass/fail), Blackboard, deadline: 25.04, 08:15</li>
     </ul>
     If passed all obligatory assignments:
     <ul>
