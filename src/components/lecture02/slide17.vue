5e95eb62c7ed6b749e3715cbc7ec2a5cf450e43d
s4bina <sabina@zoopit.no>
2021-11-22 16:42:29 +0100

    updated bem

--- src/components/lecture02/slide17.vue
+++ src/components/lecture02/slide17.vue
@@ -15,6 +15,12 @@
         <p>
           <a target="_blank" href="http://getbem.com/">Get BEM</a>
         </p>
+                <p>
+          <a target="_blank" href="https://www.youtube.com/watch?v=SLjHSVwXYq4">Why I use the BEM naming convention for my CSS</a>
+        </p>
+                        <p>
+          <a target="_blank" href="https://youtu.be/er1JEDuPbZQ?t=139">You Probably Need BEM CSS in Your Life</a>
+        </p>
       </div>
     </div>
   </div>
