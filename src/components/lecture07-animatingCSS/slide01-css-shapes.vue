8b72141dcb7f39389e4f8ad88966dcf08644f149
s4bina <sabina@zoopit.no>
2021-11-22 17:50:49 +0100

    shapes inspirations

--- src/components/lecture07-animatingCSS/slide01-css-shapes.vue
+++ src/components/lecture07-animatingCSS/slide01-css-shapes.vue
@@ -10,5 +10,12 @@
     <router-link target="_blank" to="/laboratories/laboratory06a-tasks"
       >Laboratory - CSS Shapes with border-radius</router-link
     >
+    <a href="https://sharkcoder.com/visual/shapes">https://sharkcoder.com/visual/shapes</a>
+    <a href="https://www.youtube.com/watch?v=mNKz3devFAw">https://www.youtube.com/watch?v=mNKz3devFAw</a>
+    <a href="https://www.youtube.com/watch?v=001OLmwourI">https://www.youtube.com/watch?v=001OLmwourI</a>
+    <a href="https://dev.to/poulamic/intro-to-creating-css-art-1ep5">https://dev.to/poulamic/intro-to-creating-css-art-1ep5</a>
+    <a href="https://dev.to/poulamic/pure-css-lamp-step-by-step-39i3">https://dev.to/poulamic/pure-css-lamp-step-by-step-39i3</a>
+    <a href="https://medium.com/coding-artist/a-beginners-guide-to-pure-css-images-ef9a5d069dd2">CSS Koala</a>
+    <a href="https://hacks.mozilla.org/2014/09/single-div-drawings-with-css/">https://hacks.mozilla.org/2014/09/single-div-drawings-with-css/</a>
   </div>
 </template>
