f4510dc3121763d35876497c2f44a2a18d241f1d
s4bina <sabina@zoopit.no>
2021-11-23 23:43:34 +0100

    added exercise

--- src/components/lecture07-animatingCSS/slide01-transformations.vue
+++ src/components/lecture07-animatingCSS/slide01-transformations.vue
@@ -6,18 +6,26 @@
       element. It modifies the coordinate space of the CSS visual formatting
       model.
     </p>
-    <router-link target="_blank" to="/laboratories/laboratory06b"
-      >Laboratory - CSS Transform</router-link
-    >
+    <h4>Example</h4>
     <p>
-      <span class="sources"
-        ><a
-          target="_blank"
-          href="https://developer.mozilla.org/en-US/docs/Web/CSS/transform"
-          >https://developer.mozilla.org/en-US/docs/Web/CSS/transform</a
-        ></span
+      Interactive example of transform property in 2D and 3D:<br/>
+      <a target="_blank" href="https://css-transform.moro.es/"
+        >https://css-transform.moro.es/</a
+      >
+      In pairs test and experiment with transform properties. 
+      Use your imagination on which 3D elements you can draw.  
+      Prepare four examples within your pair.
+    </p>
+    <h4>Read documentation</h4>
+    <p>
+      <a
+        target="_blank"
+        href="https://developer.mozilla.org/en-US/docs/Web/CSS/transform"
+        >Mozilla Doc: Transform</a
       >
     </p>
+    <h3>CSS Transform-origin</h3>
+    <h4>Example</h4>
     <iframe
       height="465"
       style="width: 100%"
@@ -36,12 +44,26 @@
       by Sabina (<a href="https://codepen.io/AniWeyn">@AniWeyn</a>) on
       <a href="https://codepen.io">CodePen</a>.
     </iframe>
-    <a
-      target="_blank"
-      href="https://developer.mozilla.org/en-US/docs/Web/CSS/transform-origin"
-      >Transform-origin</a
+    <h4>Read documentation</h4>
+    <p>
+      <a
+        target="_blank"
+        href="https://developer.mozilla.org/en-US/docs/Web/CSS/transform-origin"
+        >Mozilla Doc: Transform-origin</a
+      >
+    </p>
+    <h4>Laboratory</h4>
+    <router-link target="_blank" to="/laboratories/laboratory06b"
+      >CSS Transform</router-link
     >
-
-    https://css-transform.moro.es/
+    <p>
+      <span class="sources">
+        <a
+          target="_blank"
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS/transform"
+          >https://developer.mozilla.org/en-US/docs/Web/CSS/transform</a
+        ></span
+      >
+    </p>
   </div>
 </template>
