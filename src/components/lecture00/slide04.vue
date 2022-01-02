22496ff94397e22afde51f3e28d24f7b8a58674e
aniWeyn <sabina@zoopit.no>
2022-01-02 19:31:57 +0100

    fix hour

--- src/components/lecture00/slide04.vue
+++ src/components/lecture00/slide04.vue
@@ -3,7 +3,7 @@
     <h2>Course work</h2>
     3 Assignments and Individual Report
     <ul>
-      <li>Assignment 0 (not graded), Blackboard, deadline: 14.01, 08:15</li>
+      <li>Assignment 0 (not graded), Blackboard, deadline: 14.01, 23:59</li>
       <li>Assignment 1 (pass/fail), Blackboard, deadline: 31.01, 08:15</li>
       <li>Assignment 2 (pass/fail), Blackboard, deadline: 07.03, 08:15</li>
       <li>Assignment 3 (pass/fail), Blackboard, deadline: 25.04, 08:15</li>
