397462dc7e20e4a5be1b8934bd774804d4175957
aniWeyn <sabina@zoopit.no>
2022-01-07 00:32:37 +0100

    updates

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,9 @@
 <template>
   <nav id="nav" class="main-nav nav">
     <router-link to="/" class="main-nav__link">Course</router-link>
-    <router-link to="/laboratories" class="main-nav__link"
-      >Laboratories</router-link
-    >
 
     <router-link to="/about" class="main-nav__link">Assignments</router-link>
+    <router-link to="/about" class="main-nav__link">Sources</router-link>
   </nav>
 </template>
 
