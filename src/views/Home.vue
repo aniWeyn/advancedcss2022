4c557b4dcfb6f926a4bc9d8389fa0d939c304f25
aniWeyn <sabina@zoopit.no>
2022-01-13 08:04:41 +0100

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -110,7 +110,6 @@
       <h4>January 13, 2022</h4>
       <ol start="20">
         <li><strong>Guest lecture - Felipe Hefler</strong></li>
-        <!-- 
             <li>
           <router-link to="/transitions"
             >Transitions
@@ -133,7 +132,6 @@
             >Lab: Intersection Observer + Transformations, Transitions and Animations</router-link
           >
         </li>
-        -->
         </ol>
         <h4>January 14, 2022</h4>
               <ol start="22">
