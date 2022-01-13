f08d876f102d9d5e73b75d5995448442d75c010d
aniWeyn <sabina@zoopit.no>
2022-01-13 20:21:49 +0100

    uploads

--- src/components/shapes/slide01.vue
+++ src/components/shapes/slide01.vue
@@ -2,6 +2,7 @@
   <div class="slide">
     <hr />
     <h3>CSS Shapes</h3>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ihL8vd__AMA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <ul>
       <li>Open CodePen</li>
       <li>Try to solve exercises</li>
