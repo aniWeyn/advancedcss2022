4268309db3e2b5ce09c38817142d27d71c7ce458
aniWeyn <sabina@zoopit.no>
2022-03-14 12:58:50 +0100

    added Mark article

--- src/views/laboratories/Michal.vue
+++ src/views/laboratories/Michal.vue
@@ -3,5 +3,6 @@
         <h3>Michal Pietrzyk: Moderninzing UI on Enterprise level</h3>
         <a href="https://ldjam.com/users/mikoziq/games">Ludum Dare - Michal Pietrzyk</a>
         <a href="https://gitlab.com/oliasoft-open-source/react-ui-library">Oliasoft - React UI Library</a>
+        <a href="https://blog.oliasoft.com/modern-user-interface">How we're Improving our User Interface by Mark Macdonald, Software Team Lead - User Interface</a>
     </div>
 </template>
