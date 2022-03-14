87d433e3c8ba1b8ddba3fe50306be8148e920b73
aniWeyn <sabina@zoopit.no>
2022-03-14 12:57:24 +0100

    added link to gitlab

--- src/views/laboratories/Michal.vue
+++ src/views/laboratories/Michal.vue
@@ -2,5 +2,6 @@
     <div>
         <h3>Michal Pietrzyk: Moderninzing UI on Enterprise level</h3>
         <a href="https://ldjam.com/users/mikoziq/games">Ludum Dare - Michal Pietrzyk</a>
+        <a href="https://gitlab.com/oliasoft-open-source/react-ui-library">Oliasoft - React UI Library</a>
     </div>
 </template>
